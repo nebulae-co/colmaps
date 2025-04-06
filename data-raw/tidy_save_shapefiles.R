daba4c9525908842bc7a842634942241441e1b55
CruzJulian <cruz@chocolatron.cruz>
2025-04-06 18:58:26 -0500

    Tidy the shapefiles in the sf standard. Re-write the function and adding transformations to the maps. Now we have two more maps in the traditional way (with the islands in the top).

--- data-raw/tidy_save_shapefiles.R
+++ data-raw/tidy_save_shapefiles.R
@@ -1,51 +1,28 @@
 library("dplyr")
-library("rgdal")
+# library("rgdal")
 library("stringi")
-library("rgeos")
+# library("rgeos")
+library("sf")
+library("rmapshaper")
+
+
+# Generate first maps -----------------------------------------------------
 
 map <- list(municipios = NULL, departamentos = NULL)
 
-var_codigo <- c(municipios = "CODIGO_DPT", departamentos = "CODIGO_DEP")
-
 for (level in names(map)){
-  path <- list(input     = "data-raw/shapefiles/raw/",
-               rawoutput = "data-raw/shapefiles/",
-               binoutput = "data/")
+  path <- c(input     = "data-raw/shapefiles-dane/",
+               rawoutput = "data-raw/shapefiles-nebulae/")
+
+  if(!dir.exists(path["rawoutput"])){
+    dir.create(path["rawoutput"])
+  }
 
   path <- sapply(path, paste0, level)
 
-  encoding <- readLines(con = paste0(path["input"], "/", level, ".cst"),
-                        warn = FALSE)
+  map[[level]] <- st_read(path["input"], layer = level)
 
-  map[[level]] <- readOGR(dsn = path["input"], layer = level, verbose = FALSE,
-                          stringsAsFactors = FALSE, encoding = encoding)
-
-  map[[level]] <- map[[level]] %>%
-                  gSimplify(tol = .0001, topologyPreserve = TRUE) %>%
-                  SpatialPolygonsDataFrame(data = map[[level]]@data,
-                                           match.ID = FALSE)
-
-  # Set polygons ids
-  for (i in seq_len(nrow(map[[level]]))){
-    current_id <- slot(slot(map[[level]], "polygons")[[i]], "ID")
-    id_dane <- slot(map[[level]], "data")[current_id, var_codigo[level]]
-    slot(slot(map[[level]], "polygons")[[i]], "ID") <- id_dane
-  }
-
-  # Check:
-  identical(sapply(slot(map[[level]], "polygons"), slot, "ID"),
-            slot(map[[level]], "data")[[var_codigo[level]]])
-
-  # Format data
-  dots <- list(id = var_codigo[[level]],
-               id_depto = ~ CODIGO_DEP,
-               municipio = ~ stri_trans_totitle(enc2utf8(NOMBRE_MUN)),
-               depto = ~ stri_trans_totitle(enc2utf8(NOMBRE_DEP)))
-
-  if (level == "departamentos") dots <- dots[-c(2, 3)]
-
-  slot(map[[level]], "data") <- slot(map[[level]], "data") %>%
-    transmute_(.dots = dots)
+  map[[level]] <- ms_simplify(map[[level]], keep = 0.1, keep_shapes = TRUE)
 
   if(dir.exists(path["rawoutput"])){
     file.remove(dir(path["rawoutput"], full.names = TRUE))
@@ -53,9 +30,78 @@ for (level in names(map)){
     dir.create(path["rawoutput"])
   }
 
-  writeOGR(obj = map[[level]], dsn = path["rawoutput"], layer = level,
-           driver = "ESRI Shapefile", layer_options = 'ENCODING="ISO-8859-1"')
+  st_write(map[[level]], dsn = path["rawoutput"],  driver = "ESRI Shapefile",
+           layer_options = "ENCODING=UTF-8", delete_dsn = TRUE)
 
+}
+
+
+
+# Compute the insets for traditional maps. --------------------------------
+
+
+map[["municipios"]] %>%
+  st_crs() -> current_crs
+
+map[["municipios"]] %>%
+  # filter(mpio_cdpmp == "88001") -> map_sanandres # Original
+  filter(mpio_cdpmp == "88001") %>%
+  mutate(
+    geometry =
+      ((geometry - c(-81.73562, 12.59481))*10 + c(-79.01018, 12.45944)) %>%
+      st_set_crs(current_crs)
+  ) -> map_sanandres # New
+# Move to
+#   xmin = 79.01018
+#   ymax = 12.45944
+# From
+#   xmin = -81.73562
+#   ymax = 12.59481
+
+
+map[["municipios"]] %>%
+  # filter(mpio_cdpmp == "88564") -> map_providencia # Original
+  filter(mpio_cdpmp == "88564") %>%
+  mutate(
+    geometry =
+      ((geometry - c(-81.39557, 13.32034))*10 + c(-78.52459, 11.31599)) %>%
+      st_set_crs(current_crs)
+  ) -> map_providencia # New
+# Move to
+#   xmin = -78.52459
+#   ymin = 11.31599
+# From
+#   xmin = -81.39557
+#   ymin = 13.32034
+
+
+# Generate traditional maps.  ---------------------------------------------
+
+bind_rows(map_sanandres, map_providencia) -> map_inset
+
+map[["municipios"]] %>%
+  # filter(!(mpio_cdpmp %in% c("88001", "88564"))) -> map_tmp
+  filter(!(mpio_cdpmp %in% c("88001", "88564"))) %>%
+  bind_rows(map_inset) -> map[["municipios_trad"]]
+
+map_inset[["geometry"]] %>%
+  st_combine() -> geometry_inset
+
+map[["departamentos"]] %>%
+  filter(dpto_ccdgo == 88) %>%
+  st_set_geometry(geometry_inset) -> map_inset
+
+map[["departamentos"]] %>%
+  filter(!(dpto_ccdgo == 88)) %>%
+  bind_rows(map_inset) -> map[["departamentos_trad"]]
+
+
+# Guardar -----------------------------------------------------------------
+
+for (level in names(map)){
+  path <- c(binoutput = "data/")
+  path <- sapply(path, paste0, level)
   save(list = level, file = paste0(path["binoutput"], ".rda"),
        compress = "xz", envir = as.environment(map))
+
 }
